689f9e086c474a0df7a967ef3c5c19e21753b552
Nguyen Minh Tam <tamnm1032@gmail.com>
2022-08-18 23:00:42 +0700

    Read the features name

--- collect_and_clean.R
+++ collect_and_clean.R
@@ -12,3 +12,9 @@ url = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%
 download.file(url = url,destfile = "human_activity.zip")
 unzip('human_activity.zip',exdir = 'data')
 date_of_download <- date()
+
+#Read the columns' names
+path = "data/UCI HAR Dataset/"
+features <- read.table(file = paste(path,'features.txt',sep=''),header = FALSE,sep = '',
+                       col.names = c('ID','Features'))
+columns <- features$Features
