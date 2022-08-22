a505936f1c28b3d55d2d70a676e60981395b2eb8
Nguyen Minh Tam <tamnm1032@gmail.com>
2022-08-22 11:31:33 +0700

    Split the training and validation

--- machine_learning.R
+++ machine_learning.R
@@ -15,3 +15,13 @@ data <- select(data,-Subject)
 summary(data)
 percentage <- prop.table(table(data$Activity)) * 100
 cbind(freq=table(data$Activity), percentage=percentage)
+
+#Split the train and test set
+validation_index <- createDataPartition(data$Activity,
+                                        p=0.80, list=FALSE)
+validation <- data[-validation_index,]
+training <- data[validation_index,]
+
+control <- trainControl(method="cv", number=10,
+                        savePredictions = TRUE)
+metric <- "Accuracy"
