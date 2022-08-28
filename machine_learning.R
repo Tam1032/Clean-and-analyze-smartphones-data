b0e7da3562a5e3b265a0f43dc822185482d193f4
Nguyen Minh Tam <tamnm1032@gmail.com>
2022-08-28 21:56:32 +0700

    add confusion matrix drawing function

--- machine_learning.R
+++ machine_learning.R
@@ -1,6 +1,5 @@
 rm(list = ls())
 library(caret)
-library(kernlab)
 library(dplyr)
 library(ggplot2)
 
@@ -17,11 +16,34 @@ percentage <- prop.table(table(data$Activity)) * 100
 cbind(freq=table(data$Activity), percentage=percentage)
 
 #Split the train and test set
-validation_index <- createDataPartition(data$Activity,
+train_index <- createDataPartition(data$Activity,
                                         p=0.80, list=FALSE)
-validation <- data[-validation_index,]
-training <- data[validation_index,]
+validation <- data[-train_index,]
+training <- data[train_index,]
 
 control <- trainControl(method="cv", number=10,
                         savePredictions = TRUE)
 metric <- "Accuracy"
+
+#Define the function drawing the confusion matrix
+plotConfusionMatrix <- function(cfm)
+{
+  plt <- as.data.frame(cfm$table)
+  plt$Prediction <- factor(plt$Prediction, levels=rev(levels(plt$Prediction)))
+  
+  ggplot(plt, aes(x = Reference,y= Prediction, fill= Freq)) +
+    geom_tile() + geom_text(aes(label=Freq)) +
+    scale_fill_gradient(low="white", high="#009194") +
+    labs(x = "Actual",y = "Prediction") +
+    scale_x_discrete(labels=label, position = "top") +
+    scale_y_discrete(labels=rev(label))
+}
+
+#Run the KNN algorithm and plot confusion matrix
+set.seed(7)
+fit.knn <- train(Activity~., data=training, method="knn",
+                 metric=metric, trControl=control)
+knn_predictions <- predict(fit.knn, validation)
+cfm_knn <- confusionMatrix(knn_predictions, as.factor(validation$Activity))
+cfm_knn$overall['Accuracy']
+plotConfusionMatrix(cfm_knn)
